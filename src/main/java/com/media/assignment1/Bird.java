938f1ff9fb669c17f951b5cabe3aac063df91fb9
nxl_ssethy <sarita.sethy@nextlabs.com>
2020-08-05 17:39:04 +0800

    Submmited assgnment1.1 with assumptions mentioned in README.md file.

---
+++ src/main/java/com/media/assignment1/Bird.java
@@ -0,0 +1,14 @@
+package com.media.assignment1;
+
+import com.media.assignment.Animal;
+
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying.");
+    }
+
+    public void sing() {
+        System.out.println("I am singing.");
+    }
+}
+
---
+++ src/main/java/com/media/assignment1/Solution.java
@@ -0,0 +1,10 @@
+package com.media.assignment1;
+
+public class Solution {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
